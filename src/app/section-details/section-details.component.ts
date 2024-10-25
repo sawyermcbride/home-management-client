8ad3a06a62260cdc71308c459d2fde982f8f9acf
Sawyer McBride <sawyer.mcbride@outlook.com>
2024-10-24 21:46:41 -0700

    services

--- src/app/section-details/section-details.component.ts
+++ src/app/section-details/section-details.component.ts
@@ -1,19 +1,57 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { NewSectionFormType } from '../../interfaces/forms.interface';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SectionsService } from '../sections.service';
 
 @Component({
   selector: 'app-section-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './section-details.component.html',
   styleUrl: './section-details.component.css'
 })
 export class SectionDetailsComponent {
+  taskForm: FormGroup;
+  showIntervalField: boolean = false;
+  sectionsService: SectionsService = inject(SectionsService);
+
   @Input() sectionObject!: NewSectionFormType | undefined;
   @Output() backSectionEvent = new EventEmitter<boolean>(false);
+
+
+  taskSubmit() {
+    const values = this.taskForm.value;
+    console.log(this.taskForm.value);
+    this.sectionsService.addTask({...values, id: this.sectionsService.getTasks().length + 1});
+
+    console.log(this.sectionsService.getTasks());
+    this.taskForm.reset();
+  }
 
   returnSection() {
     console.log("Back section");
     this.backSectionEvent.emit(true);
+  }
+
+  constructor() {
+    this.taskForm = new FormGroup({
+      taskName: new FormControl('', Validators.required),
+      recurring: new FormControl('no', Validators.required),
+      interval: new FormControl('0')
+    });
+
+    this.taskForm.get('recurring')?.valueChanges.subscribe((value) => {
+      this.showIntervalField = value === 'yes';
+      this.toggleIntervalField();
+    });
+  }
+
+  private toggleIntervalField() {
+    const intervalControl = this.taskForm.get('interval');
+    if(this.showIntervalField) {
+      intervalControl?.enable();
+    } else {
+      intervalControl?.disable();
+    }
   }
 }
--- src/app/sections.service.ts
+++ src/app/sections.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../interfaces/objects.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SectionsService {
+  private tasks: Task[] = [];
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
 
   constructor() { }
 }
